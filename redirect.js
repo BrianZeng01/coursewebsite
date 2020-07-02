4ff081ede9f1b4e487022c5f7bd4eacd2ccc8c04
brian <brianzeng2011@hotmail.com>
2020-07-01 22:11:17 -0700

    test

---
+++ redirect.js
@@ -0,0 +1,37 @@
+var myUserEntity = {};
+function checkIfLoggedIn() {
+  if (sessionStorage.getItem("myUserEntity") == null) {
+    document.getElementById("logout").style.display = "none";
+    document.getElementById("login").style.display = "block";
+    console.log("signed out");
+  } else {
+    document.getElementById("logout").style.display = "block";
+    document.getElementById("login").style.display = "none";
+    document.getElementById("logoutmessage").innerHTML =
+      "Welcome, " + JSON.parse(sessionStorage.getItem("myUserEntity"))["Name"];
+    console.log(sessionStorage.getItem("myUserEntity"));
+  }
+}
+
+checkIfLoggedIn();
+
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  myUserEntity.Id = profile.getId();
+  myUserEntity.Name = profile.getName();
+
+  console.log("User logged in");
+  sessionStorage.setItem("myUserEntity", JSON.stringify(myUserEntity));
+  checkIfLoggedIn();
+}
+
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log("User signed out.");
+  });
+
+  sessionStorage.clear();
+  checkIfLoggedIn();
+}
+
