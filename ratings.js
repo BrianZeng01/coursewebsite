132a040caf3cf23e396d699afdc3f5e21f497eb0
brian <brianzeng2011@hotmail.com>
2020-07-11 16:56:16 -0700

    changed colors

--- ratings.js
+++ ratings.js
@@ -2,11 +2,11 @@ var overallRating = document.getElementsByClassName("ratings")
 for (var i = 0; i<overallRating.length; i++) {
     var overall = overallRating[i].innerHTML;
     if(overall >= 4) {
-        overallRating[i].style.backgroundColor = "#5CB862";
+        overallRating[i].style.backgroundColor = "#56bc5b";
     } else if(overall >= 3) {
-        overallRating[i].style.backgroundColor = "#F0AD4E";
+        overallRating[i].style.backgroundColor = "#eaa544";
     } else if(overall > 0) {
-         overallRating[i].style.backgroundColor = "#D95350";
+         overallRating[i].style.backgroundColor = "#e258560";
     } else {
          overallRating[i].style.backgroundColor = "#777777";
     }
