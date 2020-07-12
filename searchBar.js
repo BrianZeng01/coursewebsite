b87dd5f4cc334a6c4f3ae4b53455d94df1df082f
brian <brianzeng2011@hotmail.com>
2020-07-11 19:19:49 -0700

    Fixed value not being passed into form

--- searchBar.js
+++ searchBar.js
@@ -1,3 +1,13 @@
+window.addEventListener("pageshow", function (event) {
+  var historyTraversal =
+    event.persisted ||
+    (typeof window.performance != "undefined" &&
+      window.performance.navigation.type === 2);
+  if (historyTraversal) {
+    document.getElementById("search").value = "";
+  }
+});
+
 $(document).ready(function () {
   $("#search").keyup(function () {
     var query = $(this).val();
@@ -11,7 +21,7 @@ $(document).ready(function () {
           // console.log(data);
           if ((data.match(/<option>/g) || []).length == 1) {
             var button = document.getElementById("submit");
-            button.setAttribute("onclick","courseExist();");
+            button.setAttribute("onclick", "");
             button.setAttribute("type", "submit");
           }
         },
@@ -24,8 +34,4 @@ function courseDoesNotExist() {
   console.log("course does not exist");
   document.getElementById("courseDoesNotExist").innerHTML =
     "<h3>Course not found</h3>";
-}
-
-function courseExist() {
-  document.getElementById("search").value = "";
 }
