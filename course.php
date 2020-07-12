d35d226d6bcfc53e10fd9b05ddba036a1b519dcb
brian <brianzeng2011@hotmail.com>
2020-07-11 22:30:51 -0700

    Added review display, missing upvotes/flags

--- course.php
+++ course.php
@@ -88,13 +88,10 @@ $row_aggregates = mysqli_fetch_array($result_aggregates);
 // All information needed for the main(aggregated) review box
 // excluding $course_code which is above
 $course_title = $columns["course_title"];
-$total_overall = number_format((float)
-round($row_aggregates["total_overall"],1), 1, '.', '');
-$total_difficulty = number_format((float)
-round($row_aggregates["total_difficulty"],1), 1, '.', '');
+$total_overall = number_format((float) round($row_aggregates["total_overall"], 1), 1, '.', '');
+$total_difficulty = number_format((float) round($row_aggregates["total_difficulty"], 1), 1, '.', '');
 $num_take_again = $row_aggregates["num_take_again"];
 $num_of_reviews = mysqli_num_rows($reviews_result);
-number_format((float)round($r["overall"],1), 1, '.', '');
 
 echo '      <div class="overview">
                 <h1>Overview</h1>
@@ -102,22 +99,81 @@ echo '      <div class="overview">
                 <h1>' . $course_code . ' <span class="numOfReviews">
                     (' . $num_of_reviews . ' reviews)</span>
                 </h1>
-                <h2>Title: ' . $course_title . '</h2>
-                <span class="ratings scores">' . $total_overall . '</span>
-                <h1 style="display:inline;"> Overall </h1><br>
-                <span class="ratings scores">' . $total_difficulty . '</span>
-                <h1 style="display:inline;"> Difficulty </h1><br>
-                <h2>' . $num_take_again . '/' . $num_of_reviews . '  People would take this course again</h2>
-                <form action="review.php" method="GET">
-                    <input type="hidden" name="course" value="' . $course_code .'">
-                    <button class="makeReview" type="submit">Write a Review</button>
-                </form>
-                <h3>Note: Sign in to submit a review. Please be mindful
-                when submitting reviews, thank you and enjoy!</h3>
-                <hr class="underline">
-            </div>
- ';
+            <h2>Title: ' . $course_title . '</h2>
+            <span class="ratings scores">' . $total_overall . '</span>
+            <h1 style="display:inline;"> Overall </h1><br><br>
+            <span class="ratings_difficulty scores">' . $total_difficulty . '</span>
+            <h1 style="display:inline;"> Difficulty </h1><br>
+            <h2>' . $num_take_again . '/' . $num_of_reviews . '  People would take this course again</h2>
+            <form action="review.php" method="GET">
+                <input type="hidden" name="course" value="' . $course_code . '">
+                <button class="makeReview" type="submit">Write a Review</button>
+            </form>
+            <h3>Note: Sign in to submit a review. Please be mindful
+            when submitting reviews, thank you and enjoy!</h3>
+            <hr class="underline">
+        </div>
+';
 
+$output = '<div class=allReviews>
+                <h1>Reviews<h1>
+                <h3>Sort by: Work in Progress</h3>
+                <hr class="underline">
+                <ul>';
+
+while ($row = mysqli_fetch_array($reviews_result)) {
+    $votes = $row["votes"];
+    if ($row["anonymous"]) {
+        $name = "Anonymous";
+    } else {
+        $name = $row["user_first_name"];
+    }
+    $mysql_date = strtotime($row["date"]);
+    $date = date("M d/Y" , $mysql_date);
+    $difficulty = number_format((float) round($row["difficulty"], 1), 1, '.', '');
+    $overall = number_format((float) round($row["overall"], 1), 1, '.', '');
+    $professor = $row["professor"];
+    $textbook = $row["textbook"];
+    $grade = $row["grade"];
+    $year = $row["year"];
+    if ($row["take_again"]) {
+        $take_again = "Yes";
+    } else {
+        $take_again = "No";
+    }
+    $comment = $row["review_comment"];
+    $advice = $row["advice"];
+
+    $output .= '<li>
+                <div class="review">
+                    <h2 style="display:inline-block;">' . $name .'</h2>
+                    <h4 class="date">'.$date.'</h4><br>
+                    <span class="ratings scores_review">'
+        . $overall .
+        '</span>
+                    <h2 class="ratingHeaders">Overall</h2>
+                    <span class="ratings_difficulty scores_review">'
+        . $difficulty .
+        '</span>
+
+                    <h2 class="ratingHeaders">Difficulty</h2><br>
+                    <h3 class="h3seperators">Prof: ' . $professor . '</h3>
+                    <h3>Textbook: ' . $textbook . ' </h3><br>
+
+                    <h3 class="h3seperators">Grade: ' . $grade . '</h3>
+                    <h3 class="h3seperators">Year: ' . $year . '</h3>
+                    <h3>Take Again? ' . $take_again . '</h3>
+
+                    <h2 class="commentHeader">Comments</h2>
+                    <p class="comment">' . $comment . '</p>
+                    <h2 class="commentHeader">Advice</h2>
+                    <p class="comment">'.$advice.'</p>
+                </div>
+            </li>';
+}
+
+$output .= '</ul></div>';
+echo $output;
 echo '
         </div>
 
