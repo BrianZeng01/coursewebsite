98b78ac27c533443290d55c4368c41a54e92e419
brian <brianzeng2011@hotmail.com>
2020-07-11 23:33:33 -0700

    More colors added

--- ratings.js
+++ ratings.js
@@ -2,12 +2,24 @@ var overallRating = document.getElementsByClassName("ratings")
 for (var i = 0; i<overallRating.length; i++) {
     var overall = overallRating[i].innerHTML;
     if(overall >= 4) {
-        overallRating[i].style.backgroundColor = "#56bc5b";
-    } else if(overall >= 3) {
-        overallRating[i].style.backgroundColor = "#eaa544";
+        overallRating[i].style.backgroundColor = "#28b463";
+    } else if(overall >= 2.5) {
+        overallRating[i].style.backgroundColor = "#F39C12";
     } else if(overall > 0) {
-         overallRating[i].style.backgroundColor = "#e258560";
+         overallRating[i].style.backgroundColor = "#E74C3C";
     } else {
          overallRating[i].style.backgroundColor = "#777777";
+    }
+}
+
+var difficultyRating = document.getElementsByClassName("ratings_difficulty");
+for (var i = 0; i<difficultyRating.length; i++) {
+    var difficulty = difficultyRating[i].innerHTML;
+    if(difficulty >= 4) {
+        difficultyRating[i].style.backgroundColor = "#E74C3C";
+    } else if(difficulty >= 2.5) {
+        difficultyRating[i].style.backgroundColor = "#F39C12";
+    } else {
+         difficultyRating[i].style.backgroundColor = "#28b463";
     }
 }
